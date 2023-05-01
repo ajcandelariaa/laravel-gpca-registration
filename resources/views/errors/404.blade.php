f69ebde74d9919b85325bae92659541d2921b6c3
Aj Candelaria <ajajcandelaria@gmail.com>
2023-05-01 23:24:21 +0400

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -10,10 +10,10 @@
 </head>
 
 <body class="bg-gray-100">
-    <div class="flex flex-col items-center justify-center h-screen">
+    <div class="flex flex-col items-center justify-center h-screen text-center">
         <img src="{{ asset('assets/images/invoice_logo.png') }}" alt="Company Logo" >
         <h1 class="text-5xl font-bold text-gray-800 mt-6">Oops! Page not found</h1>
-        <p class="text-xl text-gray-600 mt-2">The page you're looking for doesn't exist or has been moved.</p>
+        <p class="text-xl text-gray-600 mt-4">The page you're looking for doesn't exist or has been moved.</p>
         <a href="{{ env('APP_URL') }}" class="inline-block bg-registrationPrimaryColorHover text-white px-4 py-2 rounded-full hover:bg-registrationPrimaryColor mt-6">Return to Home</a>
     </div>
 </body>
