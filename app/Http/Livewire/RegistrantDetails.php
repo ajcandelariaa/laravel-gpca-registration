48ee5f1179fba20b195acff66eec19be34fb7b88
Aj Candelaria <ajajcandelaria@gmail.com>
2023-09-25 19:48:28 +0400

    Update RegistrantDetails.php

--- app/Http/Livewire/RegistrantDetails.php
+++ app/Http/Livewire/RegistrantDetails.php
@@ -510,7 +510,7 @@ class RegistrantDetails extends Component
     public function checkUnitPrice()
     {
         // CHECK UNIT PRICE
-        dd($this->finalData);
+        // dd($this->finalData);
         if ($this->finalData['rate_type'] == "standard" || $this->finalData['rate_type'] == "Standard") {
             if ($this->finalData['pass_type'] == "fullMember") {
                 return $this->event->std_full_member_rate;
