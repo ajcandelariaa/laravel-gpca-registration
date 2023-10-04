6250494316d96760a414f972b37280b7a8247c4e
Aj Candelaria <ajajcandelaria@gmail.com>
2023-10-04 11:44:54 +0400

    v5.0.2
    1. Spouse registration first page updated

--- resources/views/livewire/registration/spouse/step/first.blade.php
+++ resources/views/livewire/registration/spouse/step/first.blade.php
@@ -1,33 +1,38 @@
 <div class="mx-5">
-    <div class="text-registrationPrimaryColor italic font-bold text-xl">
-        Spouse details
+    <div class="flex items-center justify-between">
+        <div class="text-registrationPrimaryColor italic font-bold text-xl">
+            Spouse details 
+        </div>
+        <div>
+            <a href="https://www.gpcaforum.com/spouse-program/" target="_blank" class="text-white bg-registrationPrimaryColorHover text-md rounded-md py-1 px-4 hover:bg-registrationPrimaryColor">Click here to view itineraries</a>
+        </div>
     </div>
 
     <div class="mt-5 grid grid-cols-2 gap-y-3 gap-x-5">
         {{-- ROW 1 --}}
         <div class="col-span-2 space-y-2">
             <div class="text-registrationPrimaryColor">
-                Select which day(s) you want to attend <span class="text-red-500">*</span>
+                Select which day(s) you want to attend <span class="text-red-500">*</span> 
             </div>
             <div>
                 <div class="flex items-center gap-2">
                     <input type="checkbox" wire:model.lazy="selectedDays" value="1">
-                    <label>December 4, 2023 (Monday)</label>
+                    <label>December 4, 2023 (Monday) - <span class="font-semibold">$ 200.00</span></label>
                 </div>
                 
                 <div class="flex items-center gap-2">
                     <input type="checkbox" wire:model.lazy="selectedDays" value="2">
-                    <label>December 5, 2023 (Tuesday)</label>
+                    <label>December 5, 2023 (Tuesday) - <span class="font-semibold">$ 220.00</span></label>
                 </div>
                 
                 <div class="flex items-center gap-2">
                     <input type="checkbox" wire:model.lazy="selectedDays" value="3">
-                    <label>December 6, 2023 (Wednesday)</label>
+                    <label>December 6, 2023 (Wednesday) - <span class="font-semibold">$ 200.00</span></label>
                 </div>
                 
                 <div class="flex items-center gap-2">
                     <input type="checkbox" wire:model.lazy="selectedDays" value="4">
-                    <label>December 7, 2023 (Thursday)</label>
+                    <label>December 7, 2023 (Thursday) - <span class="font-semibold">$ 200.00</span></label>
                 </div>
 
                 @error('selectedDays')
