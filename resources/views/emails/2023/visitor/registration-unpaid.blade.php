660692e68d27b8cc7deca4f7a2a6be2943f6e54d
Aj Candelaria <ajajcandelaria@gmail.com>
2023-12-01 00:08:04 +0400

    v6.7.1
    1. AF visitor email notification updated

--- resources/views/emails/2023/visitor/registration-unpaid.blade.php
+++ resources/views/emails/2023/visitor/registration-unpaid.blade.php
@@ -1,16 +1,11 @@
 <x-mail::message>
 <p class="sub">Dear {{ $details['name'] }},</p>
 
-<p class="sub" style="margin-top: 15px;">Warm greetings from GPCA!</p>
+<p class="sub" style="margin-top: 15px;">Greetings from GPCA!</p>
 
-<p class="sub" style="margin-top: 15px;">We appreciate your registration for the <a href="{{ $details['eventLink'] }}" target="_blank">{{ $details['eventName'] }}</a>, scheduled to take place from {{ $details['eventDates'] }}, at the {{ $details['eventLocation'] }}.</p>
+<p class="sub" style="margin-top: 15px;">Thank you for registering as a visitor for the <a href="https://www.gpcaforum.com/" target="_blank">17<sup>th</sup> Annual GPCA Forum</a>, which will be held from <strong>4-6 December 2023</strong>, at the <strong>{{ $details['eventLocation'] }}</strong>. Please note that the opening ceremony of the forum will commence on <strong>3<sup>rd</sup> December</strong> from <strong>16:00-18:30</strong>. Access to this exclusive event is restricted to VIP, delegate and exhibitor passes only.</p>
 
-<p class="sub" style="margin-top: 15px; color: red;">Kindly note that your registration is not yet confirmed. Please settle your payment through bank transfer prior to the event to avoid any inconvenience onsite. </p>
-
-<p class="title" style="margin-top: 20px;">Badge Collection:</p>
-<p class="sub">Visitors can begin collecting their badges on December 3<sup>rd</sup> at the designated registration desk on-site. To ensure a seamless and convenient experience for our esteemed visitors, we have designated the following locations:</p>
-
-<p class="sub" style="margin-top: 15px;"><span class="subtitle">Exhibition Halls 4 to 5 on the Ground Floor:</span> This area is designated for Youth Forum participants, exhibitors, and visitors to pick up their badges.</p>
+<p class="sub" style="margin-top: 15px; color: red;">Kindly note that your registration is not yet confirmed. Please settle your payment through bank transfer prior to the event to avoid any inconvenience onsite.</p>
 
 <p class="title" style="margin-top: 20px;">Visitor Information:</p>
 <p class="sub">Full name: {{  $details['name'] }}</p>
@@ -28,20 +23,30 @@ Download invoice
 </x-mail::button>
 @endif
 
-<p class="sub" style="margin-top: 15px;">We look forward to your participation in the event and hope that your experience at the {{ $details['eventName'] }} will be both enriching and insightful.</p>
+<p class="subtitle" style="margin-top: 15px; text-decoration: underline;">Badge Collection</p>
+<p class="sub" style="margin-top: 5px;">Visitors can start collecting their badges on 3<sup>rd</sup> December 2023, from 16:00-17:00 onwards located at the Ground Level Exhibition Halls 5 & 6.</p>
 
-<p class="sub" style="margin-top: 15px;">Should you have any further inquiries or require additional information, please do not hesitate to reach out to us.</p>
+<p class="subtitle" style="margin-top: 15px; text-decoration: underline;">Visitor Pass Access</p>
+<p class="sub" style="margin-top: 5px;">Your visitor passes grants access to the following forum features:</p>
+<ul class="event-list">
+<li>Solutions Xchange program</li>
+<li>Exhibition Halls</li>
+<li>Cultural Majlis</li>
+<li>Sustainability District</li>
+<li>Publication Showcase</li>
+<li>Industry Journey</li>
+</ul>
 
-<p class="sub" style="margin-top: 15px;">Thank you once again for your registration, and we eagerly anticipate your presence at the event.</p>
+<p class="sub" style="margin-top: 15px; color: red;">To upgrade your visitor, pass and gain full access to the forum, please contact forumregistration@gpca.org.ae.</p>
 
-<p class="title" style="margin-top: 30px;">GENERAL INFORMATION</p>
+<p class="subtitle" style="margin-top: 15px; text-decoration: underline;">Visa Information</p>
+<p class="sub" style="margin-top: 5px;">For travelers with passports issued by India, Pakistan, Iran, Thailand, and Ukraine, please reach out to Cozmo Travel https://www.gpcaforum.com/travel-accomodation/ for visa assistance if you have booked your accommodation through them.</p>
 
-<p class="subtitle" style="margin-top: 15px;">Qatar Visa</p>
-<p class="sub">For travellers with passports issued by India, Pakistan, Iran, Thailand, and Ukraine, please reach out to Cozmo Travel <a href="https://www.gpcaforum.com/travel-accomodation/">https://www.gpcaforum.com/travel-accomodation/</a> for visa assistance if you have booked your accommodation through them.</p>
+<p class="sub" style="margin-top: 15px;">For other non-GCC nationals applying for a visa and who have booked their accommodation through our travel partner should contact Cozmo Travel to obtain a hotel confirmation letter, a requirement for your visa applications.</p>
 
-<p class="sub" style="margin-top: 15px;">Other non-GCC nationalities applying for a visa and who have booked their accommodation through our travel partner should approach Cozmo Travel to obtain a hotel confirmation letter, which is a visa application requirement.</p>
+<p class="sub" style="margin-top: 15px;">We look forward to your participation in the event and hope that your experience at the <strong>17<sup>th</sup> Annual GPCA Forum</strong> will be both enriching and insightful. Should you have any further inquiries or require additional information, please do not hesitate to reach out to us.</p>
 
-<p class="sub" style="margin-top: 15px;">We look forward to welcoming you to this exceptional event!</p>
+<p class="sub" style="margin-top: 15px;">Thank you once again for your registration, and we look forward to welcoming you to this exceptional event!</p>
 
 <p class="sub" style="margin-top: 15px;">Best regards,</p>
 <p class="sub">GPCA Team</p>
