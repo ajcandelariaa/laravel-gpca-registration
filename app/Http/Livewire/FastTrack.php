3c1a486aeb496d442dd1d8de7989c3472efc2349
Aj Candelaria <ajajcandelaria@gmail.com>
2024-01-08 11:48:10 +0400

    Update FastTrack.php

--- app/Http/Livewire/FastTrack.php
+++ app/Http/Livewire/FastTrack.php
@@ -262,7 +262,7 @@ class FastTrack extends Component
 
     public function getGLFConfirmedDelegates()
     {
-        $eventCategory = "GLF";
+        $eventCategory = "ANC";
 
         $event = Events::where('category', $eventCategory)->select('id', 'category', 'year')->first();
         $mainDelegates = MainDelegates::where('event_id', $event->id)->get();
