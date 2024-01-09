111e8b0dd978957a7d366fe09f6b55baedcf1ea0
Aj Candelaria <ajajcandelaria@gmail.com>
2024-01-09 10:03:16 +0400

    Update registration-paid.blade.php

--- resources/views/emails/2024/glf/registration-paid.blade.php
+++ resources/views/emails/2024/glf/registration-paid.blade.php
@@ -1,32 +1,26 @@
 <x-mail::message>
 <p class="sub">Dear {{ $details['name'] }},</p>
 
-<p class="sub" style="margin-top: 15px;">Greetings from GPCA!</p>
+<p class="sub" style="margin-top: 15px;">Thank you for registering for the 4<sup>th</sup> GPCA Leaders Forum, an exclusive C-level event and to be held under the theme <strong><em>“Charting the Energy Evolution Pathways in the GCC”</em></strong>. We are delighted to confirm your successful registration and appreciate your commitment to participating in this strategic forum.</p>
 
-<p class="sub" style="margin-top: 15px;">Thank you for registering to attend the <a href="{{ $details['eventLink'] }}" target="_blank">{{ $details['eventName'] }}</a> taking place from 28 Feburary 2024 at {{ $details['eventLocation'] }}.</p>
+<p class="sub" style="margin-top: 20px;"><strong>Event Details:</strong></p>
+<p class="sub" style="margin-top: 5px;"><strong>Date:</strong> 28 February 2024</p>
+<p class="sub"><strong>Time:</strong> 09:00-14:00</p>
+<p class="sub"><strong>Venue:</strong> Kempinski Central Avenue, Dubai, UAE</p>
 
-<p class="sub" style="margin-top: 15px;">Your registration has been confirmed, and we are pleased to provide you with the booking confirmation below for your reference.</p>
+<p class="sub" style="margin-top: 20px;"><strong>Key information you shouldn’t miss:</strong></p>
+<ul class="event-list">
+<li style="margin-top: 5px;"><strong>Collection of badges</strong><br>Upon your arrival, kindly make your way to the registration desk to collect your event badge. This badge will serve as your access pass, allowing entry to all sessions and networking events scheduled throughout the day.</li>
+<li style="margin-top: 15px;"><strong>Agenda and speakers</strong><br>A copy of the agenda can be viewed <a href="https://www.gpca.org.ae/gpca-leaders-forum/agenda/" target="_blank">here</a>, and physical copies will be distributed onsite. Get ready for an engaging panel discussions and thought-provoking presentations that promise to enrich your experience during the forum.</li>
+<li style="margin-top: 15px;"><strong>Networking opportunities</strong><br>Maximize the networking sessions to establish connections with fellow C-level executives, exchange ideas, and explore potential collaborations.</li>
+</ul>
 
-<p class="title" style="margin-top: 20px;">Delegate Information:</p>
-<p class="sub">Full name: {{  $details['name'] }}</p>
-<p class="sub">Job title: {{  $details['jobTitle'] }}</p>
-<p class="sub">Company name: {{  $details['companyName'] }}</p>
-@if ($sendInvoice)
-<p class="sub">Amount paid: $ {{ number_format($details['amountPaid'], 2, '.', ',') }}</p>
-@endif
-<p class="sub">Transaction ID: {{  $details['transactionId'] }}</p>
+<p class="sub" style="margin-top: 15px;">For any queries and further assistance, please contact forumregistration@gpca.org.ae or call +971 4 451 0666 ext. 153.</p>
 
-@if ($sendInvoice)
-<br>
-<x-mail::button :url="$details['invoiceLink']" color="registration">
-Download invoice
-</x-mail::button>
-@endif
+<p class="sub" style="margin-top: 15px;">Thank you for choosing to be a part of the 4<sup>th</sup> GPCA Leaders Forum. Your participation will contribute to the success of this forum, where industry leaders gather to engage in insightful discussions, gain access to exclusive insights, and establish valuable connections.</p>
 
-<p class="sub" style="margin-top: 15px;">To collect your badge onsite, please present your business card as a reference to facilitate the verification process. To request changes, kindly respond to this email at the earliest, to rectify your badge. </p>
+<p class="sub" style="margin-top: 15px;">The GPCA looks forward to hosting you for a day of insightful discussions, leadership, and networking excellence.</p>
 
-<p class="sub" style="margin-top: 15px;">For the latest updates on the event, please visit the event website at <a href="{{ $details['eventLink'] }}" target="_blank">{{ $details['eventName'] }}</a>.</p>
-
-<p class="sub" style="margin-top: 15px;">Best regards,</p>
+<p class="sub" style="margin-top: 15px;">Warm Regards,</p>
 <p class="sub">GPCA Team</p>
 </x-mail::message>
