18370b44b3a081879a629547f96c750dea9b4365
Aj Candelaria <ajajcandelaria@gmail.com>
2024-02-08 09:44:52 +0400

    v6.11.3
    1. SCC email notifs updated

--- resources/views/emails/2024/scc/registration-unpaid.blade.php
+++ resources/views/emails/2024/scc/registration-unpaid.blade.php
@@ -45,7 +45,7 @@ Download invoice
 
 <p class="sub" style="margin-top: 15px;">Stay updated on upcoming GPCA events and industry news by following our <a href="https://www.linkedin.com/company/gulf-petrochemicals-and-chemicals-association-gpca-/">LinkedIn Page</a>. You can also connect with us on our official social media accounts: <a href="https://twitter.com/GulfPetChem">Twitter</a>, <a href="https://www.instagram.com/gulfpetchem/">Instagram</a>, <a href="https://www.facebook.com/GulfPetChem?fref=ts">Facebook</a>, and <a href="https://www.youtube.com/user/GPCAorg">Youtube</a>. </p>
 
-<p class="sub" style="margin-top: 15px;">Thank you, and we look forward to welcoming you to the premier gathering of plastic industry professionals in the region.</p>
+<p class="sub" style="margin-top: 15px;">For the latest updates on the event, please visit the event website at <a href="{{ $details['eventLink'] }}">GPCA Supply Chain Conference</a>.</p>
 
 <p class="sub" style="margin-top: 15px;">Kind Regards,</p>
 <p class="sub">GPCA Team</p>
