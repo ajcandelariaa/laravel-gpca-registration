da8980bd5068c2c4560d77b05e539e2f1f92c53b
Aj Candelaria <ajajcandelaria@gmail.com>
2024-05-09 11:52:28 +0400

    Update scan-qr.blade.php

--- resources/views/livewire/admin/events/scan-qr/scan-qr.blade.php
+++ resources/views/livewire/admin/events/scan-qr/scan-qr.blade.php
@@ -16,8 +16,8 @@
                 <div
                     class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-filter backdrop-blur-md flex flex-col items-center justify-center z-50">
                     <div class="relative overflow-hidden rounded-lg border-4 border-gray-300">
-                        <div class="hidden absolute inset-0 bg-gradient-to-r from-registrationPrimaryColor to-registrationSecondaryColor opacity-0 animate-scan m-2"
-                            id="scan-animation"></div>
+                        {{-- <div class="hidden absolute inset-0 bg-gradient-to-r from-registrationPrimaryColor to-registrationSecondaryColor opacity-0 animate-scan m-2"
+                            id="scan-animation"></div> --}}
                         <video id="preview"></video>
                     </div>
 
