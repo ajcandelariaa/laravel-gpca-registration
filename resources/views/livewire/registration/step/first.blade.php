0c4fe66b963e6988c7dbfec0e1ba4f892c8334c1
Aj Candelaria <ajajcandelaria@gmail.com>
2024-06-05 08:14:13 +0400

    v8.3.5
    1. Faheem's job title updated

--- resources/views/livewire/registration/step/first.blade.php
+++ resources/views/livewire/registration/step/first.blade.php
@@ -49,7 +49,7 @@
                 @endif --}}
 
                 <p class="mt-5">For inquiries or to speak with a member of our team, please contact <strong>Faheem
-                        Chowdhury</strong>, <em>Head of Events & Sales</em>, at <a
+                        Chowdhury</strong>, <em>Head of Events</em>, at <a
                         href="mailto:faheem@gpca.org.ae">faheem@gpca.org.ae</a> or call +971 4 451 0666 ext. 122.</p>
             </div>
         @endif
