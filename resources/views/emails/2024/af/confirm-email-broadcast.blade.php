47611993eceb05553dc294726a614978a2ced636
Aj Candelaria <ajajcandelaria@gmail.com>
2024-11-30 18:36:20 +0400

    Update confirm-email-broadcast.blade.php

--- resources/views/emails/2024/af/confirm-email-broadcast.blade.php
+++ resources/views/emails/2024/af/confirm-email-broadcast.blade.php
@@ -6,7 +6,7 @@
 <p class="sub" style="margin-top: 15px;">We are pleased to confirm your registration. Below is a summary of your booking confirmation, including details for badge collection. Please bring a valid ID and your registration confirmation to collect your badge.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong>Badge Collection</strong></p>
-<p class="sub">Attendees may begin collecting their badges on 1 December at the designated registration desk on-site. To ensure a seamless and convenient experience for our valued attendees, please note the details regarding badge collection onsite.</p>
+<p class="sub">Attendees can collect their badges from 1 December at the designated areas below. To ensure a seamless and convenient experience for our valued attendees, please note the following collection points.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong>TO FIND THE EXACT LOCATION WHERE YOU CAN COLLECT YOUR BADGE ONSITE KINDLY CLICK <a href="https://gpcaregistration.com/event/AF/20/digital-helper" target="_blank">HERE</a></strong></p>
 
@@ -15,7 +15,7 @@
 <table width="540" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
     <tr>
         <td width="210">
-            <p class="sub" style="text-decoration: underline"><strong>Date and timings</strong></p>
+            <p class="sub" style="text-decoration: underline"><strong>Dates and timings</strong></p>
         </td>
         <td width="20"></td>
         <td width="310">
@@ -36,7 +36,7 @@
                         <p class="sub" style="font-size: 11px;">1 December</p>
                     </td>
                     <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 16:00 <br> <strong>(ONLY DELEGATES)</strong></p>
+                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 16:00 <br> <strong>(DELEGATES ONLY)</strong></p>
                     </td>
                 </tr>
                 <tr valign="top">
@@ -126,7 +126,7 @@
                 </tr>
                 <tr valign="top">
                     <td width="125" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">Fast track counter <br> <strong>(ONLY FOR CONFERENCE DELEGATES)</strong></p>
+                        <p class="sub" style="font-size: 11px;">Fast track counters <br> <strong>(FOR CONFERENCE DELEGATES ONLY)</strong></p>
                     </td>
                     <td width="185" style="border: 1px solid black;" align="center">
                         <p class="sub" style="text-align: center; font-size: 11px;">Exhibition Hall Concourse</p>
@@ -146,11 +146,7 @@
 <p class="sub">Badge type: {{  $details['badgeType'] }}</p>
 <p class="sub">Transaction ID: {{  $details['transactionId'] }}</p>
 
-{{-- <img src="data:image/png;base64, {!! base64_encode(
-    QrCode::format('png')->size(200)->generate($details['qrCodeForPrint']),
-) !!} "> --}}
-
-<p class="sub" style="margin-top: 15px;"><strong>General Information</strong></p>
+<p class="sub" style="margin-top: 15px;"><strong>Networking Luncheons</strong></p>
 <p class="sub">All registered attendees attending the 18<sup>th</sup> Annual GPCA Forum are warmly invited to join us for the Networking Luncheons served from Day 2 to Day 4 of the event.</p>
 
 <br>
@@ -158,7 +154,7 @@
 <table width="540" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
     <tr>
         <td width="210">
-            <p class="sub" style="text-decoration: underline"><strong>Date and timings</strong></p>
+            <p class="sub" style="text-decoration: underline"><strong>Dates and timings</strong></p>
         </td>
         <td width="20"></td>
         <td width="310">
@@ -176,26 +172,10 @@
             <table align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
                 <tr valign="top">
                     <td width="90" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">1 December</p>
-                    </td>
-                    <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 16:00</p>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <td width="90" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">2 December</p>
-                    </td>
-                    <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 18:00</p>
-                    </td>
-                </tr>
-                <tr valign="top">
-                    <td width="90" style="border: 1px solid black;">
                         <p class="sub" style="font-size: 11px;">3 December</p>
                     </td>
                     <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">07:00 – 15:00</p>
+                        <p class="sub" style="text-align: center; font-size: 11px;">13:00 – 14:30</p>
                     </td>
                 </tr>
                 <tr valign="top">
@@ -203,7 +183,7 @@
                         <p class="sub" style="font-size: 11px;">4 December</p>
                     </td>
                     <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">07:00 – 15:00</p>
+                        <p class="sub" style="text-align: center; font-size: 11px;">13:15 – 14:45</p>
                     </td>
                 </tr>
                 <tr valign="top">
@@ -211,7 +191,7 @@
                         <p class="sub" style="font-size: 11px;">5 December</p>
                     </td>
                     <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">08:00 – 13:00</p>
+                        <p class="sub" style="text-align: center; font-size: 11px;">13:15 – 15:00</p>
                     </td>
                 </tr>
             </table>
@@ -265,8 +245,9 @@
 </table>
 
 <p class="sub" style="margin-top: 15px;"><strong>Welcome Dinner</strong></p>
-<p class="sub"><strong>(December 4/ 19:00 – 22:00)</strong></p>
-<p class="sub"><strong>Royal Opera House Oman (cutt.ly/reHf3tjK)</strong></p>
+<p class="sub"><em>Sponsored by: OQ</em></p>
+<p class="sub"><strong>3 December 2024 | 19:00 – 22:00</strong></p>
+<p class="sub"><strong>Seafront Maidan, Royal Opera House Oman</strong></p>
 
 <p class="sub" style="margin-top: 15px;">As we embark on this transformative journey together, the Welcome dinner serves as the perfect occasion to kickstart the Forum, allowing you to connect with peers and lay the groundwork to fruitful discussion throughout the event.</p>
 
@@ -275,12 +256,13 @@
 <p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
 
 <p class="sub" style="margin-top: 15px;"><strong>Gala Dinner</strong></p>
-<p class="sub"><strong>(December 5/19:00-22:00)</strong></p>
-<p class="sub"><strong>LA VIE Club (cutt.ly/beHguAT4)</strong></p>
+<p class="sub"><em>Sponsored by: SABIC</em></p>
+<p class="sub"><strong>4 December 2024 | 19:00 – 22:00</strong></p>
+<p class="sub"><strong>LA VIE Golf Club</strong></p>
 
 <p class="sub" style="margin-top: 15px;">Prepare for an unforgettable evening at the Gala Dinner, a perfect opportunity to unwind, celebrate achievements, and foster valuable connections within the chemical and petrochemical industries. Join us for an evening that promises not only unforgettable memories but also connections that last well beyond the Forum.</p>
 
-<p class="sub" style="margin-top: 15px;">The dress code is National dress/smart casual.</p>
+<p class="sub" style="margin-top: 15px;">Dress code: National dress/smart casual.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
 
