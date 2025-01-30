d05ebcf84c42fd9216f912498c713aefb91eba31
Aj Candelaria <ajajcandelaria@gmail.com>
2025-01-30 09:19:20 +0400

    Update first.blade.php

--- resources/views/livewire/registration/step/first.blade.php
+++ resources/views/livewire/registration/step/first.blade.php
@@ -52,9 +52,11 @@
                         Chowdhury</strong>, <em>Head of Events</em>, at <a
                         href="mailto:faheem@gpca.org.ae">faheem@gpca.org.ae</a> or call +971 4 451 0666 ext. 122.</p>
 
-                <p class="font-bold mt-5">If you have registered within the early bird time frame, payment must be
-                    completed before the deadline. Otherwise, the registration fee will automatically reflect the
-                    standard rate. </p>
+                @if ($eventCategory == 'PC')
+                    <p class="font-bold mt-5">If you have registered within the early bird time frame, payment must be
+                        completed before the deadline. Otherwise, the registration fee will automatically reflect the
+                        standard rate. </p>
+                @endif
             </div>
         @endif
 
@@ -212,7 +214,10 @@
                 Chowdhury</strong>, <em>Head of Events</em>, at <a
                 href="mailto:faheem@gpca.org.ae">faheem@gpca.org.ae</a> or call +971 4 451 0666 ext. 122.</p>
 
-        <p class="col-span-2 font-bold mt-5">If you have registered within the early bird time frame, payment must be completed
-            before the deadline. Otherwise, the registration fee will automatically reflect the standard rate. </p>
+        @if ($eventCategory == 'PC')
+            <p class="col-span-2 font-bold mt-5">If you have registered within the early bird time frame, payment must
+                be completed before the deadline. Otherwise, the registration fee will automatically reflect the
+                standard rate. </p>
+        @endif
     @endif
 </div>
