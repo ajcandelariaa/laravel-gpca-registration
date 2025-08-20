322d54b6746d579da60214b9f393b6c5ae2e3ffb
Aj Candelaria <ajajcandelaria@gmail.com>
2025-08-20 11:40:55 +0400

    IPAW email notif added

---
+++ resources/views/emails/2025/ipaw/registration-unpaid.blade.php
@@ -0,0 +1,34 @@
+<x-mail::message>
+<p class="sub" >Dear {{ $details['name'] }},</p>
+
+<p class="sub" style="margin-top: 15px;">Greetings from GPCA!</p>
+
+<p class="sub" style="margin-top: 15px;">Thank you for registering to attend the <a href="{{ $details['eventLink'] }}" target="_blank">{{ $details['eventName'] }}</a>, taking place from {{ $details['eventDates'] }} at {{ $details['eventLocation'] }}</p>
+
+<p class="sub" style="margin-top: 15px; color: red;"><strong>Please be advised that your registration is not yet confirmed. To avoid any inconvenience during onsite badge collection, please settle your payment or contact our finance team at <a href="mailto:analee@gpca.org.ae">analee@gpca.org.ae</a>. </strong></p>
+
+<p class="sub" style="margin-top: 15px;">Your registration details as follows:</p>
+
+<p class="title" style="margin-top: 20px;">Delegate Information:</p>
+<p class="sub">Full name: {{  $details['name'] }}</p>
+<p class="sub">Job title: {{  $details['jobTitle'] }}</p>
+<p class="sub">Company name: {{  $details['companyName'] }}</p>
+@if ($sendInvoice)
+<p class="sub">Amount paid: $ {{ number_format($details['amountPaid'], 2, '.', ',') }}</p>
+@endif
+<p class="sub">Transaction ID: {{  $details['transactionId'] }}</p>
+
+@if ($sendInvoice)
+<br>
+<x-mail::button :url="$details['invoiceLink']" color="registration">
+Download invoice
+</x-mail::button>
+@endif
+
+<p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>Collection of badges</strong></p>
+
+<p class="sub" style="margin-top: 15px;">Upon your arrival, kindly make your way to the registration desk to collect your event badge. This badge will serve as your access pass, allowing entry to all sessions and networking events scheduled throughout the day.</p>
+
+<p class="sub" style="margin-top: 15px;">Best Regards,</p>
+<p class="sub">GPCA Team</p>
+</x-mail::message>
