03bd5cc5a0fe2d5e41778c0d3d58c72cfa67337a
Aj Candelaria <ajajcandelaria@gmail.com>
2025-11-30 12:23:51 +0400

    Update pending-email-broadcast.blade.php

--- resources/views/emails/2025/af/youth/broadcast/2/pending-email-broadcast.blade.php
+++ resources/views/emails/2025/af/youth/broadcast/2/pending-email-broadcast.blade.php
@@ -1,28 +1,244 @@
 <x-mail::message>
 <p class="sub">Dear Delegate,</p>
 
-<p class="sub" style="margin-top: 15px;">Before we meet in Bahrain for the GPCA Youth Forum (8–11 December 2025), here is the key thing to know from day one:</p>
+<p class="sub" style="margin-top: 15px;">One week to go. A direct reminder for all youth delegates:</p>
 
-<p class="sub" style="margin-top: 15px;">The Youth Zone <strong>is the place to be</strong>.</p>
+<p class="sub" style="margin-top: 15px;">The Youth Zone is where your Youth Forum program and features take place.</p>
 
-<p class="sub" style="margin-top: 15px;">This is where your program runs, where your community gathers, and where the Youth Forum experience comes to life.</p>
+<p class="sub" style="margin-top: 15px;">Plan to spend your time there daily, because that is where the youth experience is designed to happen.</p>
 
-<p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>What you will find in the Youth Zone: </strong></p>
-<ul class="event-list">
-    <li style="margin-top: 5px;">Your daily youth delegate program and features</li>
-    <li>Interactive challenges and hands-on activations</li>
-    <li>Meaningful networking with CEOs, mentors, and peers across the region</li>
-    <li>Easy access to mentors and speakers throughout the day</li>
-    <li>The GPCA Youth Council members on site for instant support</li>
-    <li>Daily highlights and next-up reminders</li>
-</ul>
+<p class="sub" style="margin-top: 15px; font-size: 18px;"><strong>Schedule: </strong></p>
+
+<p class="sub" style="margin-top: 5px;"><strong>DAY 1 – Monday 8 December</strong></p>
+<table width="540" align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">Time</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px; font-weight: bold;">Activity</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">16:00 – 18:30</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Attend the inaugural of the 18<sup>th</sup> Annual GPCA Forum</p>
+        </td>
+    </tr>
+</table>
+
+<p class="sub" style="margin-top: 15px;"><strong>DAY 2 – Tuesday 9 December</strong></p>
+<table width="540" align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">Time</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px; font-weight: bold;">Activity</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">14:30 – 14:40</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Opening & Curtain Raiser</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">14:40 – 14:50</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Welcome Remarks</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">14:50 – 15:30</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;"><strong>Youth Majlis – CEO Panel:</strong> Future-Proofing the Petrochemical Industry</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">15:30 – 16:45</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;"><strong>Fireside Chat:</strong> Professional Presence & Career Etiquette</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">16:45-17:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Closing Day 1: Chemicals & Innovation: Building the Future Together</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">19:00-22:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Welcome Dinner</p>
+        </td>
+    </tr>
+</table>
+
+<p class="sub" style="margin-top: 15px;"><strong>DAY 3 – Wednesday 10 December</strong></p>
+<table width="540" align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">Time</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px; font-weight: bold;">Activity</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">09:00-09:10</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Opening by GPCA Youth Council Vice Chair</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">09:10-09:40</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Youth Speak: From Earth to the Stars: Youth Leadership, Innovation, and Building Pathways to Space</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">09:40-10:10</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Youth Speak: Empowering Educators: Building the Future Through Knowledge and Purpose</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">10:10-10:40</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Youth Speak: Shaping Global Solutions on Plastics and Climate: A Youth Negotiator’s Perspective.</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">10:40-11:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Networking Break</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">11:00-11:15</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">MoU signing ceremony with top regional academic institutes</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">11:30-12:45</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Youth Council Dialogue</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">12:45-14:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Networking lunch</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">14:00-15:30</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Youth-Leaders Debate Powered by SYLC</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">19:00–22:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Gala Dinner</p>
+        </td>
+    </tr>
+</table>
+
+<p class="sub" style="margin-top: 15px;"><strong>DAY 4 – Thursday 11 December</strong></p>
+<table width="540" align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">Time</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px; font-weight: bold;">Activity</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">09:00-10:30</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Speed Mentorship</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">10:30-11:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Networking Break</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">11:00-12:30</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Career Lab: Interview Edition</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">12:30-13:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Attend Forum's closing at main Plenary</p>
+        </td>
+    </tr>
+    <tr valign="top">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px; font-weight: bold;">13:00-15:00</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Networking Lunch</p>
+        </td>
+    </tr>
+</table>
 
 <p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>Location and Opening Hours</strong></p>
 <p class="sub">📍 Youth Zone: <strong>Exhibition World Bahrain + HALL 10</strong></p>
 <p class="sub">🕒 Open daily: <strong>8:30AM to 5:00PM</strong></p>
 
-<p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>Join the Delegates WhatsApp Group now</strong></p>
-<p class="sub">This is our live channel for reminders, highlights, Youth Zone schedules, and direct assistance. You can also use it to interact with fellow delegates and reach the GPCA team quickly.</p>
+<p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>Do this now: </strong></p>
+<p class="sub">Join the Delegates WhatsApp Group so you do not miss any Youth Zone updates. You will receive live reminders and can ask for support at any time.</p>
 
 <p class="sub" style="margin-top: 15px;">👉 Join here: <a href="https://chat.whatsapp.com/EhNsWlHwyQ1LQBpZwHl7mO">YF25 Delegate Whatsapp Group</a></p>
 
