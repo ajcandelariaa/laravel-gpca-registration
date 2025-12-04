766003ef449d6b3397025325b55415a956715172
Aj Candelaria <ajajcandelaria@gmail.com>
2025-12-04 18:27:26 +0300

    af email broadcast added

---
+++ resources/views/emails/2025/af/pending-email-broadcast.blade.php
@@ -0,0 +1,187 @@
+<x-mail::message>
+<p class="sub">Warm greetings from GPCA!</p>
+
+<p class="sub" style="margin-top: 15px;">It is with great pleasure to welcome you to the <a href="https://www.gpcaforum.com/" target="_blank">19<sup>th</sup> Annual GPCA Forum</a>, scheduled to take place from <strong>8-11 December 2025</strong> at the <strong>Exhibition World Bahrain, Sakhir, Kingdom of Bahrain</strong>.</p>
+
+<p class="sub" style="margin-top: 15px; color:red;">Please note that badges will only be issued to paid registrations. Kindly ensure that your registration dues are settled and paid before 6<sup>th</sup> December to facilitate smooth badge collection onsite. Onsite payment is available through cash (preferred) or credit card.</p>
+
+<p class="sub" style="margin-top: 15px;"><strong>Badge Collection</strong></p>
+<p class="sub">Badge pickup will begin on <strong>7<sup>th</sup> December</strong> at the <strong>designated registration desk on-site</strong>. We encourage you to collect your badge early to avoid any delays and to help streamline your entry into the event.</p>
+
+<p class="sub" style="margin-top: 15px;"><strong>TO FIND THE EXACT LOCATION WHERE YOU CAN COLLECT YOUR BADGE ONSITE KINDLY CLICK <a href="https://gpcaregistration.com/event/AF/32/digital-helper" target="_blank">HERE</a></strong></p>
+
+<br>
+
+<table width="540" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+    <tr>
+        <td width="210">
+            <p class="sub" style="text-decoration: underline"><strong>Date and timings</strong></p>
+        </td>
+        <td width="20"></td>
+        <td width="310">
+            <p class="sub" style="text-decoration: underline"><strong>Locations</strong></p>
+        </td>
+    </tr>
+    <tr height="10">
+        <td colspan="3"></td>
+    </tr>
+</table>
+
+<table width="540" border="0" align="center" cellpadding="0" cellspacing="0" style="border-collapse: collapse;">
+    <tr valign="top">
+        <td width="210">
+            <table align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+                <tr valign="top">
+                    <td width="90" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">7 December</p>
+                    </td>
+                    <td width="120" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 18:00</p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="90" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">8 December</p>
+                    </td>
+                    <td width="120" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 18:00</p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="90" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">9 December</p>
+                    </td>
+                    <td width="120" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">07:30 – 16:30</p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="90" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">10 December</p>
+                    </td>
+                    <td width="120" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">07:30 – 16:30</p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="90" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">11 December</p>
+                    </td>
+                    <td width="120" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">08:00 – 15:00</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td width="20"></td>
+        <td width="310">
+            <table align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">VIP badges</p>
+                    </td>
+                    <td width="185" rowspan="6" style="border: 1px solid black;" align="center">
+                        <p class="sub" style="text-align: center; font-size: 11px;">Main Concourse</p>
+                    </td>
+                </tr>
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">Premiere delegates</p>
+                    </td>
+                </tr>
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">Delegates</p>
+                    </td>
+                </tr>
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">Youth Forum</p>
+                    </td>
+                </tr>
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">Exhibitors</p>
+                    </td>
+                </tr>
+                <tr valign="middle">
+                    <td width="125" style="border: 1px solid black;">
+                        <p class="sub" style="font-size: 11px;">Media</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<p class="sub" style="margin-top: 15px;"><strong><em>** Media badge to be collected from 8 December</em></strong></p>
+
+<p class="sub" style="margin-top: 15px;"><strong>Delegate Details</strong></p>
+<p class="sub">Full name: {{  $details['fullName'] }}</p>
+<p class="sub">Job title: {{  $details['jobTitle'] }}</p>
+<p class="sub">Company name: {{  $details['companyName'] }}</p>
+<p class="sub">Badge type: {{  $details['badgeType'] }}</p>
+<p class="sub">Transaction ID: {{  $details['transactionId'] }}</p>
+
+<p class="sub" style="margin-top: 15px;"><strong>General Information</strong></p>
+<p class="sub">All registered attendees attending the 19<sup>th</sup> Annual GPCA Forum are warmly invited to join us for the Networking Luncheons served from Day 2 to Day 4 of the event.</p>
+
+<p class="sub" style="margin-top: 15px; text-decoration: underline;"><strong>Locations</strong></p>
+
+<br>
+
+<table width="540" align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
+    <tr valign="middle">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px;">VIPs</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">VIP Majlis</p>
+        </td>
+    </tr>
+    <tr valign="middle">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px;">Premiere delegates</p>
+        </td>
+        <td width="430" style="border: 1px solid black;">
+            <p class="sub" style="font-size: 11px;">Exhibition Hall 1</p>
+        </td>
+    </tr>
+    <tr valign="middle">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px;">Delegates</p>
+        </td>
+    </tr>
+    <tr valign="middle">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px;">Youth Forum</p>
+        </td>
+    </tr>
+    <tr valign="middle">
+        <td width="110" style="border: 1px solid black;" align="center">
+            <p class="sub" style="text-align: center; font-size: 11px;">Exhibitors</p>
+        </td>
+    </tr>
+</table>
+
+<p class="sub" style="margin-top: 15px;"><strong>Welcome Dinner</strong></p>
+<p class="sub"><strong>(December 9/ 19:00 – 22:00)</strong></p>
+<p class="sub"><strong><a href="https://maps.app.goo.gl/ybEYqY8cXKaizWqe7" target="_blank">Heritage Village (Askar)</a></strong></p>
+
+<p class="sub" style="margin-top: 15px;">Join us for an unforgettable evening, brimming with exquisite cuisine and engaging conversations set in an informal ambiance. Begin cultivating new connections in a relaxed and welcoming atmosphere.</p>
+
+<p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
+
+<p class="sub" style="margin-top: 15px;"><strong>Gala Dinner</strong></p>
+<p class="sub"><strong>(December 10/ 19:00-22:00)/strong></p>
+<p class="sub"><strong><a href="https://maps.app.goo.gl/opKoaBtGoDBNj4SG7" target="_blank">Bahrain National Theatre</a></strong></p>
+
+<p class="sub" style="margin-top: 15px;">Join us for an evening of splendor, featuring gourmet cuisine, live performances, and the opportunity to network with industry leaders. It’s a night dedicated to acknowledging achievements and fostering meaningful connections.</p>
+
+<p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
+
+<p class="sub" style="margin-top: 15px;">We look forward to welcoming you to this exceptional event!</p>
+
+<p class="sub" style="margin-top: 15px;">Best regards,</p>
+<p class="sub">GPCA Team</p>
+</x-mail::message>
