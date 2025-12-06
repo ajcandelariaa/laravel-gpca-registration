7702d5ca37bd02704c5b56d08edbb648bd32688a
Aj Candelaria <ajajcandelaria@gmail.com>
2025-12-06 20:50:27 +0300

    AF email broadcast updated

--- resources/views/emails/2025/af/pending-email-broadcast.blade.php
+++ resources/views/emails/2025/af/pending-email-broadcast.blade.php
@@ -1,14 +1,16 @@
 <x-mail::message>
 <img src="https://www.gpcaforum.com/wp-content/uploads/2025/08/email-notif-banner-v2.jpg">
 
-<p class="sub" style="margin-top: 15px;">Warm greetings from GPCA!</p>
+<p class="sub" style="margin-top: 15px;">Dear {{  $details['fullName'] }},</p>
+
+<p class="sub" style="margin-top: 15px;">Warm greetings from GPCA</p>
 
 <p class="sub" style="margin-top: 15px;">It is with great pleasure to welcome you to the <a href="https://www.gpcaforum.com/" target="_blank">19<sup>th</sup> Annual GPCA Forum</a>, scheduled to take place from <strong>8-11 December 2025</strong> at the <strong>Exhibition World Bahrain, Sakhir, Kingdom of Bahrain</strong>.</p>
 
-<p class="sub" style="margin-top: 15px; color:red;">Please note that badges will only be issued to paid registrations. Kindly ensure that your registration dues are settled and paid before 6<sup>th</sup> December to facilitate smooth badge collection onsite. Onsite payment is available through cash (preferred) or credit card.</p>
+<p class="sub" style="margin-top: 15px; color: red;">Please note that badges will only be issued to paid registrations. Kindly ensure that your registration dues are settled and paid to facilitate smooth badge collection onsite. Onsite payment is available through cash (preferred) or credit card. </p>
 
 <p class="sub" style="margin-top: 15px;"><strong>Badge Collection</strong></p>
-<p class="sub">Badge pickup will begin on <strong>7<sup>th</sup> December</strong> at the <strong>designated registration desk on-site</strong>. We encourage you to collect your badge early to avoid any delays and to help streamline your entry into the event.</p>
+<p class="sub">Collection of badges will start on <strong>7<sup>th</sup> December</strong> and can be collected at the <strong>designated registration desk on-site</strong>. We encourage you to collect your badge early to avoid any delays and ease your event access onsite.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong>TO FIND THE EXACT LOCATION WHERE YOU CAN COLLECT YOUR BADGE ONSITE KINDLY CLICK <a href="https://gpcaregistration.com/event/AF/32/digital-helper" target="_blank">HERE</a></strong></p>
 
@@ -46,7 +48,7 @@
                         <p class="sub" style="font-size: 11px;">8 December</p>
                     </td>
                     <td width="120" style="border: 1px solid black;" align="center">
-                        <p class="sub" style="text-align: center; font-size: 11px;">12:00 – 18:00</p>
+                        <p class="sub" style="text-align: center; font-size: 11px;">10:00 – 17:00</p>
                     </td>
                 </tr>
                 <tr valign="top">
@@ -80,7 +82,7 @@
             <table align="center" cellpadding="5" cellspacing="5" style="border-collapse: collapse; border: 1px solid black;">
                 <tr valign="middle">
                     <td width="125" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">VIP badges</p>
+                        <p class="sub" style="font-size: 11px;">VIP badge</p>
                     </td>
                     <td width="185" rowspan="6" style="border: 1px solid black;" align="center">
                         <p class="sub" style="text-align: center; font-size: 11px;">Main Concourse</p>
@@ -88,12 +90,12 @@
                 </tr>
                 <tr valign="middle">
                     <td width="125" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">Premiere delegates</p>
+                        <p class="sub" style="font-size: 11px;">Premiere Delegate</p>
                     </td>
                 </tr>
                 <tr valign="middle">
                     <td width="125" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">Delegates</p>
+                        <p class="sub" style="font-size: 11px;">Delegate</p>
                     </td>
                 </tr>
                 <tr valign="middle">
@@ -103,7 +105,7 @@
                 </tr>
                 <tr valign="middle">
                     <td width="125" style="border: 1px solid black;">
-                        <p class="sub" style="font-size: 11px;">Exhibitors</p>
+                        <p class="sub" style="font-size: 11px;">Exhibitor</p>
                     </td>
                 </tr>
                 <tr valign="middle">
@@ -116,7 +118,7 @@
     </tr>
 </table>
 
-<p class="sub" style="margin-top: 15px;"><strong><em>** Media badge to be collected from 8 December</em></strong></p>
+<p class="sub" style="margin-top: 15px;"><strong><em>** Collection of Media badges will begin on the 8<sup>th</sup> of December.</em></strong></p>
 
 <p class="sub" style="margin-top: 15px;"><strong>Delegate Details</strong></p>
 <p class="sub">Full name: {{  $details['fullName'] }}</p>
@@ -126,7 +128,7 @@
 <p class="sub">Transaction ID: {{  $details['transactionId'] }}</p>
 
 <p class="sub" style="margin-top: 15px;"><strong>General Information</strong></p>
-<p class="sub">All registered attendees attending the 19<sup>th</sup> Annual GPCA Forum are warmly invited to join us for the Networking Luncheons served from Day 2 to Day 4 of the event.</p>
+<p class="sub">All registered attendees attending the 19<sup>th</sup> Annual GPCA Forum are invited to join us for the Networking Lunch served from Day 2 to Day 4 of the event</p>
 
 <p class="sub" style="margin-top: 15px; margin-bottom: 5px; text-decoration: underline;"><strong>Locations</strong></p>
 
@@ -136,12 +138,12 @@
             <p class="sub" style="text-align: center; font-size: 11px;">VIPs</p>
         </td>
         <td width="430" style="border: 1px solid black;">
-            <p class="sub" style="text-align: center; font-size: 11px;">VIP Majlis</p>
+            <p class="sub" style="text-align: center; font-size: 11px;">VIP Majlis (Exhibition Hall 9)</p>
         </td>
     </tr>
     <tr valign="middle">
         <td width="110" style="border: 1px solid black;" align="center">
-            <p class="sub" style="text-align: center; font-size: 11px;">Premiere delegates</p>
+            <p class="sub" style="text-align: center; font-size: 11px;">Premiere Delegate</p>
         </td>
         <td width="430" rowspan="4" style="border: 1px solid black;">
             <p class="sub" style="text-align: center; font-size: 11px;">Exhibition Hall 1</p>
@@ -149,7 +151,7 @@
     </tr>
     <tr valign="middle">
         <td width="110" style="border: 1px solid black;" align="center">
-            <p class="sub" style="text-align: center; font-size: 11px;">Delegates</p>
+            <p class="sub" style="text-align: center; font-size: 11px;">Delegate</p>
         </td>
     </tr>
     <tr valign="middle">
@@ -159,28 +161,28 @@
     </tr>
     <tr valign="middle">
         <td width="110" style="border: 1px solid black;" align="center">
-            <p class="sub" style="text-align: center; font-size: 11px;">Exhibitors</p>
+            <p class="sub" style="text-align: center; font-size: 11px;">Exhibitor</p>
         </td>
     </tr>
 </table>
 
 <p class="sub" style="margin-top: 15px;"><strong>Welcome Dinner</strong></p>
-<p class="sub"><strong>(December 9/ 19:00 – 22:00)</strong></p>
-<p class="sub"><strong><a href="https://maps.app.goo.gl/ybEYqY8cXKaizWqe7" target="_blank">Heritage Village (Askar)</a></strong></p>
+<p class="sub"><strong>(9 December | 19:00 – 22:00)</strong></p>
+<p class="sub">Venue: <strong><a href="https://maps.app.goo.gl/ybEYqY8cXKaizWqe7" target="_blank">Heritage Village (Askar)</a></strong></p>
 
 <p class="sub" style="margin-top: 15px;">Join us for an unforgettable evening, brimming with exquisite cuisine and engaging conversations set in an informal ambiance. Begin cultivating new connections in a relaxed and welcoming atmosphere.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
 
 <p class="sub" style="margin-top: 15px;"><strong>Gala Dinner</strong></p>
-<p class="sub"><strong>(December 10/ 19:00-22:00)</strong></p>
-<p class="sub"><strong><a href="https://maps.app.goo.gl/opKoaBtGoDBNj4SG7" target="_blank">Bahrain National Theatre</a></strong></p>
+<p class="sub"><strong>(10 December | 19:00-22:00)</strong></p>
+<p class="sub">Venue: <strong><a href="https://maps.app.goo.gl/opKoaBtGoDBNj4SG7" target="_blank">Bahrain National Theatre</a></strong></p>
 
 <p class="sub" style="margin-top: 15px;">Join us for an evening of splendor, featuring gourmet cuisine, live performances, and the opportunity to network with industry leaders. It’s a night dedicated to acknowledging achievements and fostering meaningful connections.</p>
 
 <p class="sub" style="margin-top: 15px;"><strong><em>To access the venue, please present your delegate pass.</em></strong></p>
 
-<p class="sub" style="margin-top: 15px;">We look forward to welcoming you to this exceptional event!</p>
+<p class="sub" style="margin-top: 15px;">We look forward to welcoming you to this exceptional event! See you in Bahrain.</p>
 
 <p class="sub" style="margin-top: 15px;">Best regards,</p>
 <p class="sub">GPCA Team</p>
